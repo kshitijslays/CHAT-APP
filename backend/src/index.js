9b30cccef85a516c781829f33833019c70d5ecd1
kshitijslays <rajkshitij876@gmail.com>
2025-06-19 23:17:52 +0530

    message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connect } from "mongoose";
 
 dotenv.config()
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:",+ PORT);
